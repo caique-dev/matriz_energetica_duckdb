75d983c48e54a747aeba289be050a5f3ed642521
Caique Andrade <cpinheroca@gmail.com>
2025-06-15 22:55:52 -0300

    iniciando pop

--- code/funcoes.py
+++ code/funcoes.py
@@ -3,19 +3,18 @@ import csv
 def extrai_populacao():
     file = open('../data/dados_processados/populacao.csv')
 
-def extrai_csv(file):
+def extrai_csv(file, info1, info2, info3):
     infos = csv.reader(open(file))
     _ = next(infos)
     values = []
     for row in infos:
         if (row[1] != '' and len(row[1]) <= 3):
             value = []
-            value.append(row[2]) # ano
-            value.append(row[1]) # sigla
-            value.append(row[3]) # info especifica
+            value.append(row[info1]) # info1
+            value.append(row[info2]) # info2
+            value.append(row[info3]) # info3
             # print(value)
             values.append(value)
 
     return values
 
-extrai_csv('./data/dados_processados/country-and-continent-codes-list-csv.csv')
